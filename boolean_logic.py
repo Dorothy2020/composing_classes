87b25f6bd81448b290134a7574c8ab94ca127e5b
Dorothy2020 <wangaarraakoth@gmail.com>
2023-10-02 14:14:14 +0300

    feat:work on boolean logic & conditions

---
+++ boolean_logic.py
@@ -0,0 +1,25 @@
+if (1 == 1) and (2 + 2 > 3):
+
+  print("true")
+
+else:
+
+  print("false")
+
+
+
+
+if not True:
+
+   print("1")
+
+elif not (1 + 1 == 3):
+
+   print("2")
+
+else:
+
+   print("3")
+
+
+
---
+++ conditions.py
@@ -0,0 +1,22 @@
+#Else statement
+
+if 1 + 1 == 2:
+
+   if 2 * 2 == 8:
+       print("if")
+
+   else:
+
+      print("else")
+
+x = 10
+
+y = 20
+
+if x > y:
+
+    print(x + y)
+
+else:
+
+    print(y - x)
